9af6e3b03687544d17391c8b2019f10f28bb2f18
Bernhard Posselt <dev@bernhard-posselt.com>
2019-12-26 20:22:00 +0100

    use js moustache for rendering

--- index.ts
+++ index.ts
@@ -1,37 +1,48 @@
 import {countResults, generateNumber, parseFormula, roll} from './roller';
+import * as Mustache from 'mustache';
 
-Hooks.on('renderChatMessage', (_, html, msg) => {
-    const content = msg.message.content;
-    if (content.startsWith('/l5r ')) {
+const tpl = `
+<div>
+    <div>
+        {{#each rolls as roll}}
+        <img alt="{{roll}}" src="public/modules/ffg-roller/images/{{roll.imageName}}.png" width="36" height="36">
+        {{/each}}
+    </div>
+    <hr>
+    <div>
+        <ul>
+            {{#if results.opportunity > 0}}
+            <li>Opportunity: {{results.opportunity}}</li>
+            {{/if}}
+            {{#if results.strife > 0}}
+            <li>Opportunity: {{results.strife}}</li>
+            {{/if}}
+            {{#if results.successes > 0}}
+            <li>Opportunity: {{results.successes}}</li>
+            {{/if}}
+            {{#if results.failures > 0}}
+            <li>Opportunity: {{results.failures}}</li>
+            {{/if}}
+        </ul>
+    </div>
+</div>
+`;
+
+Hooks.on('preCreateChatMessage', (_, data) => {
+    const content = data.content;
+    if (content !== undefined && content.startsWith('/l5r ')) {
         const formula = content.replace(/\/l5r /g, '');
         console.info(`Rolling formula ${formula}`);
-
-        const chatData: ChatData = {
-            user: game.user.id,
-            type: CHAT_MESSAGE_TYPES.OTHER,
-            content: '',
-        };
 
         try {
             const parsedFormula = parseFormula(formula);
             const rolls = roll(parsedFormula.rings, parsedFormula.skills, generateNumber);
-            html.hide();
-
-            const template = 'public/modules/l5r-roller/templates/roll-dialog.html';
-            const templateData: TemplateData = {
+            data.content = Mustache.render(tpl, {
                 rolls: rolls,
                 results: countResults(rolls),
-            };
-            renderTemplate(template, templateData)
-                .then((content) => {
-                    chatData['content'] = content;
-                    ChatMessage.create(chatData, {displaySheet: false});
-                });
+            });
         } catch (e) {
-            chatData['content'] = e.message;
-            ChatMessage.create(chatData, {displaySheet: false});
+            data.content = e.message;
         }
-
-
     }
 });
--- types.d.ts
+++ types.d.ts
@@ -1,44 +1,10 @@
-interface User {
-    id: string
-}
-
-interface Game {
-    user: User
-}
-
-interface ChatData {
-    user: string;
-    type: string;
-    content: string;
-}
-
-interface RenderProps {
-    displaySheet: boolean;
-}
-
-interface ChatMessage {
-    create(chatData: ChatData, renderProps: RenderProps): void;
-}
-
-type TemplateData = {
-    [key: string]: any
-}
-
 interface MessageContents {
     content: string;
 }
 
-interface Message {
-    message: MessageContents;
-}
-
 interface Hooks {
-    on(event: string, callback: (app: any, html: JQuery, msg: Message) => void): void;
+    on(event: string, callback: (message_class: string, msg: MessageContents) => void): void;
 }
 
-declare var game: Game;
-declare var CHAT_MESSAGE_TYPES: any;
-declare var ChatMessage: ChatMessage;
 declare var Hooks: Hooks;
 
-declare function renderTemplate(template: string, templateData: TemplateData): Promise<string>;
