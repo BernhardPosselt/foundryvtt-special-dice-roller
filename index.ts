12763eb3aafb50383ec69c3ba4f7a308bd108c9c
Bernhard Posselt <dev@bernhard-posselt.com>
2019-12-26 18:41:50 +0100

    write errors to stream

--- index.ts
+++ index.ts
@@ -5,24 +5,33 @@ Hooks.on('renderChatMessage', (_, html, msg) => {
     if (content.startsWith('/l5r ')) {
         const formula = content.replace(/\/l5r /g, '');
         console.debug(`Rolling formula ${formula}`);
-        const parsedFormula = parseFormula(formula);
-        const rolls = roll(parsedFormula.rings, parsedFormula.skills, generateNumber);
-        html.hide();
 
-        const template = 'public/modules/l5r-roller/templates/roll-dialog.html';
-        const templateData: TemplateData = {
-            rolls: rolls,
-            results: countResults(rolls),
-        };
         const chatData: ChatData = {
             user: game.user.id,
             type: CHAT_MESSAGE_TYPES.OTHER,
-            content: ''
+            content: '',
         };
-        renderTemplate(template, templateData)
-            .then((content) => {
-                chatData['content'] = content;
-                ChatMessage.create(chatData, {displaySheet: false});
-            });
+
+        try {
+            const parsedFormula = parseFormula(formula);
+            const rolls = roll(parsedFormula.rings, parsedFormula.skills, generateNumber);
+            html.hide();
+
+            const template = 'public/modules/l5r-roller/templates/roll-dialog.html';
+            const templateData: TemplateData = {
+                rolls: rolls,
+                results: countResults(rolls),
+            };
+            renderTemplate(template, templateData)
+                .then((content) => {
+                    chatData['content'] = content;
+                    ChatMessage.create(chatData, {displaySheet: false});
+                });
+        } catch (e) {
+            chatData['content'] = e.message;
+            ChatMessage.create(chatData, {displaySheet: false});
+        }
+
+
     }
 });
