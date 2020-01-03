77ccce7c6524d6e280a4c088f5eaa96b5897310a
Bernhard Posselt <dev@bernhard-posselt.com>
2020-01-03 14:37:14 +0100

    preserve re-roll-order

--- src/template.ts
+++ src/template.ts
@@ -3,7 +3,14 @@ const base = `
     <div>
         <form>
             {{#rolls}}
-            <input type="checkbox" style="background-image: url('modules/special-dice-roller/public/images/{{system}}/{{imageName}}.png')" name="roll{{rollIndex}}" data-die="{{die}}" data-face="{{face}}" disabled/>
+            <input
+                class="{{#wasReRoll}}special-dice-roller-was-re-roll{{/wasReRoll}}"
+                type="checkbox"
+                style="background-image: url('modules/special-dice-roller/public/images/{{system}}/{{imageName}}.png')"
+                name="roll{{rollIndex}}"
+                data-die="{{die}}"
+                data-face="{{face}}"
+            />
             {{/rolls}}
             {{#canReRoll}}
             <button class="special-dice-roller-reroll" data-roller="{{system}}">re-roll selected</button>
