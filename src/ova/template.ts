fed7acda2c871d60b7e2dd2b9422b572cd87f3a2
Richard Lively <richielively@gmail.com>
2022-02-01 01:33:12 -0500

    Simplified the OVA template and now using OVA images for the selected die face

--- src/ova/template.ts
+++ src/ova/template.ts
@@ -4,34 +4,22 @@ const tpl = `
         <div class="dice-result">
             {{^negative}}
             <div class="dice-tooltip">
-                <section class="tooltip-part">
-                    <div class="dice">
-                        <ol class="dice-rolls">
-                            {{#key1}}<li class="roll die d6 max">1</li>{{/key1}}
-                            {{#key2}}<li class="roll die d6 max">2</li>{{/key2}}
-                            {{#key3}}<li class="roll die d6 max">3</li>{{/key3}}
-                            {{#key4}}<li class="roll die d6 max">4</li>{{/key4}}
-                            {{#key5}}<li class="roll die d6 max">5</li>{{/key5}}
-                            {{#key6}}<li class="roll die d6 max">6</li>{{/key6}}
-                        </ol>
-                    </div>
-                </section>
+                {{#key1}}<img class="ova-best" src="modules/special-dice-roller/public/images/ova/ova-1.png" alt="1 was the best die face" title="1 was the best die face">{{/key1}}
+                {{#key2}}<img class="ova-best" src="modules/special-dice-roller/public/images/ova/ova-2.png" alt="2 was the best die face" title="2 was the best die face">{{/key2}}
+                {{#key3}}<img class="ova-best" src="modules/special-dice-roller/public/images/ova/ova-3.png" alt="3 was the best die face" title="3 was the best die face">{{/key3}}
+                {{#key4}}<img class="ova-best" src="modules/special-dice-roller/public/images/ova/ova-4.png" alt="4 was the best die face" title="4 was the best die face">{{/key4}}
+                {{#key5}}<img class="ova-best" src="modules/special-dice-roller/public/images/ova/ova-5.png" alt="5 was the best die face" title="5 was the best die face">{{/key5}}
+                {{#key6}}<img class="ova-best" src="modules/special-dice-roller/public/images/ova/ova-6.png" alt="6 was the best die face" title="6 was the best die face">{{/key6}}
             </div>
             {{/negative}}
             {{#negative}}
             <div class="dice-tooltip">
-                <section class="tooltip-part">
-                    <div class="dice">
-                        <ol class="dice-rolls">
-                            {{#key1}}<li class="roll die d6 min">1</li>{{/key1}}
-                            {{#key2}}<li class="roll die d6 min">2</li>{{/key2}}
-                            {{#key3}}<li class="roll die d6 min">3</li>{{/key3}}
-                            {{#key4}}<li class="roll die d6 min">4</li>{{/key4}}
-                            {{#key5}}<li class="roll die d6 min">5</li>{{/key5}}
-                            {{#key6}}<li class="roll die d6 min">6</li>{{/key6}}
-                        </ol>
-                    </div>
-                </section>
+                {{#key1}}<img class="ova-worst" src="modules/special-dice-roller/public/images/ova/ova-1.png" alt="1 was the worst die face" title="1 was the worst die face">{{/key1}}
+                {{#key2}}<img class="ova-worst" src="modules/special-dice-roller/public/images/ova/ova-2.png" alt="2 was the worst die face" title="2 was the worst die face">{{/key2}}
+                {{#key3}}<img class="ova-worst" src="modules/special-dice-roller/public/images/ova/ova-3.png" alt="3 was the worst die face" title="3 was the worst die face">{{/key3}}
+                {{#key4}}<img class="ova-worst" src="modules/special-dice-roller/public/images/ova/ova-4.png" alt="4 was the worst die face" title="4 was the worst die face">{{/key4}}
+                {{#key5}}<img class="ova-worst" src="modules/special-dice-roller/public/images/ova/ova-5.png" alt="5 was the worst die face" title="5 was the worst die face">{{/key5}}
+                {{#key6}}<img class="ova-worst" src="modules/special-dice-roller/public/images/ova/ova-6.png" alt="6 was the worst die face" title="6 was the worst die face">{{/key6}}
             </div>
             {{/negative}}
             {{#negative}}
